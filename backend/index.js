bd0e245b95afbbe39ca6cbdf73dbec00c0c4ed4b
Bishal4225 <kumarbishal4225@gmail.com>
2024-10-02 09:49:44 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { connectDB } from "./db/connectDB.js";
+const app =express();
+const PORT =process.env.PORT || 5000;
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config();
+
+
+// allows us to parse incomming requests : req.body
+app.use(express.json()); 
+
+app.use("/api/auth",authRoutes);
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is runing on port:",PORT)
+});
