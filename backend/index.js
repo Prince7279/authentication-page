f9960a5c0eaa415f8e34d75ea7f53be4b78441b2
Bishal4225 <kumarbishal4225@gmail.com>
2024-10-22 00:04:56 +0530

    backend complete

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,18 @@ import { connectDB } from "./db/connectDB.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js"
+
 
 dotenv.config();
 
 
 // allows us to parse incomming requests : req.body
 app.use(express.json()); 
+
+app.use(cookieParser()); // allows us to parse incoming cookies
+
 
 app.use("/api/auth",authRoutes);
 app.listen(PORT, () => {
